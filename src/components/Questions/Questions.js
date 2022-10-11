967f85f8ff4bc2c430170ad342fd49217d690daa
Tamimul Islam <hmtamim24@gmail.com>
2022-10-11 23:02:12 +0600

    quiz page funtionality added

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Questions.css'
 
-const Questions = ({ question }) => {
-    const {id, question: title, options, correctAnswer} = question;
-    // console.log(question);
+const Questions = ({ question , setCorrect }) => {
+    const {id, question: title, options, correctAnswer, total} = question;
+    const [clicked, setClicked] = useState(null)
+    const checkAnswer = (e) => {
+        setClicked(true)
+        const isCorrect = e.target.innerText === correctAnswer
+        if (isCorrect) {
+            setCorrect()
+            e.target.classList.add('green')
+        } else {
+            e.target.classList.add('red')
+
+        }
+    }
     return (
         <div>
-            <div className="border rounded p-3 m-2 max-w-2xl mx-auto">
+            
+            <div className="border rounded p-3 m-2 max-w-2xl mx-auto bg-slate-50">
             <h2 className="text-xl font-bold">{title}</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 max-w-xl mx-auto gap-3 my-8">
+            <div className={`${clicked && 'pointer-events-none'} grid grid-cols-1 md:grid-cols-2 max-w-xl mx-auto gap-3 my-8`}>
                 {
-                    options.map(option => <button className="py-4 px-4 w-full rounded bg-violet-600 inline-block font-bold text-white">{option}</button>)
+                        options.map(option => <button onClick={(e) => checkAnswer(e)} className={`bg-violet-600 py-4 px-4 w-full rounded inline-block font-bold text-white`}>{option}</button>)
                 }
             </div>
         </div>
--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Questions from '../Questions/Questions';
 
 const QuizDetails = () => {
     const quiz = useLoaderData()
-    const {questions} = quiz.data
-    console.log(quiz.title);
+    const { questions, total } = quiz.data
+    const [correctAnswer, setCorrectAnswer] = useState(0)
+    const setCorrect = () => {
+        setCorrectAnswer((prev) => prev + 1)
+    }
     return (
         <div className="mt-8 p-4">
-            
+            <h2 className="font-bold text-xl mb-8">Correct Answers: {correctAnswer}/{total}</h2>
            
                 {
-                    questions.map((question, index) => <Questions key={index} question={question}/>)
-                }
+                questions.map((question, index) => <Questions key={index} question={question} setCorrect={setCorrect} />)
+            }
+            
+            <button className="py-2 px-4 rounded bg-violet-700 text-white font-bold my-8">Submit Answers</button>
             
         </div>
     );
